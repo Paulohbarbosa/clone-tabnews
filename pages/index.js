502a2e6f98e49f1b85efa5a0acefad77fc2c9ce6
Paulo Barbosa <paulobarbosah@hotmail.com>
2026-01-17 21:46:01 -0300

    Adicionou Stilo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
+        //coloca o fundo preto e a fonte branca
+       
+        <div style={{ display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            height: '100vh', 
+            backgroundColor: 'black', 
+            color: 'white',
+            fontSize: '24px',
+            fontWeight: 'bold',
+            fontFamily: 'Arial, sans-serif'
+            }}>
             <h1>Clonando o TabNews</h1>
         </div>
     )
