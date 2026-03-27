4dfc502c070a9535b965d7a9e4c8c070be6da9f5
Paulo Barbosa <paulobarbosah@hotmail.com>
2026-03-27 17:44:26 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto completo para aprender programação, com o https//curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        //coloca o fundo preto e a fonte branca
-       
-        <div style={{ display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            height: '100vh', 
-            backgroundColor: 'black', 
-            color: 'white',
-            fontSize: '24px',
-            fontWeight: 'bold',
-            fontFamily: 'Arial, sans-serif'
-            }}>
-            <h1>Clonando o TabNews</h1>
-        </div>
-    )
+  function handleHomeClick() {
+    console.log("Home");
+  }
+  return (
+    //coloca o fundo preto e a fonte branca
+
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "black",
+        color: "white",
+        fontSize: "24px",
+        fontWeight: "bold",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1>Clonando o TabNews</h1>
+    </div>
+  );
 }
