9f8253bac21f9dcea11b9d3e4b1748c8a46f737f
Paulo Barbosa <paulobarbosah@hotmail.com>
2026-05-20 13:44:58 -0300

    add  endpoint

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function soma(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        return 'Erro';
-    }
-    return a + b;
-}
-
-exports.soma = soma;
--- tests/calculadora.test.js
+++
@@ -1,9 +0,0 @@
-const calculadora = require('../models/calculadora');
-
-test('soma 1+1 que deveria ser 2', () => {
-    expect(calculadora.soma(1, 1)).toBe(2);
-});
-
-test('soma palavra + 1 deveria ser erro', () => {
-    expect(calculadora.soma('palavra', 1)).toBe('Erro');
-})
