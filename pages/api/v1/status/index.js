6f47da83794cca281b6931e8d34ff036d34506c9
Paulo Barbosa <paulobarbosah@hotmail.com>
2026-05-27 18:49:42 -0300

    add database.js and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-    response.status(200).json({ status: 'Estamos On-line!! É isso ai!!' });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).send("Estamos On-line!! É isso ai!!");
 }
 
 export default status;
