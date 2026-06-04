656f3505a0ae5d5a1b8c32d002ec31050ebc49f6
Paulo Barbosa <paulobarbosah@hotmail.com>
2026-06-04 15:28:54 -0300

    fix: set explicit content-type header and correct encoding in status API response

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "../../../../infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response.status(200).send("Estamos On-line!! É isso ai!!");
+  response.setHeader("content-type", "text/html; charset=utf-8").status(200).send("Estamos On-line!! É isso aí!!");
 }
 
 export default status;
